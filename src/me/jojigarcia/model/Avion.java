e5b3bc67f1480ace36d88b19120de5a227a96ce1
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-04 13:02:17 +0200

    - Añadida la clase avión con sus propiedades, constructores y getters&setters. - Falta repasar g&s y modificar constructores para que se apoyen en estos.

--- src/me/jojigarcia/model/Avion.java
+++ src/me/jojigarcia/model/Avion.java
@@ -1,5 +1,7 @@
 package me.jojigarcia.model;
 
+import java.util.Date;
+
 /**
  * Created by joaquinjimenezgarcia on 4/5/17.
  */
@@ -7,6 +9,62 @@ public class Avion {
     private String codVuelo;
     private String aerolinea;
     private int velocidad;
-    private date fechaHora;
+    private Date fechaHora;
     private double distancia;
+
+    public Avion() {
+        this.codVuelo = "Desconocida";
+        this.aerolinea = "Desconocida";
+        this.velocidad = 0;
+        this.fechaHora = new Date();
+        this.distancia = 0;
+    }
+
+    public Avion(String codVuelo, String aerolinea, int velocidad, Date fechaHora, double distancia) {
+        this.codVuelo = codVuelo;
+        this.aerolinea = aerolinea;
+        this.velocidad = velocidad;
+        this.fechaHora = fechaHora;
+        this.distancia = distancia;
+    }
+
+    public String getCodVuelo() {
+        return codVuelo;
+    }
+
+    public void setCodVuelo(String codVuelo) {
+        this.codVuelo = codVuelo;
+    }
+
+    public String getAerolinea() {
+        return aerolinea;
+    }
+
+    public void setAerolinea(String aerolinea) {
+        this.aerolinea = aerolinea;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
 }
