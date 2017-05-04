3cae432158d1fa698daaa99ec480f8449c6de974
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-04 12:55:13 +0200

    Initial commit

---
+++ src/me/jojigarcia/model/Avion.java
@@ -0,0 +1,12 @@
+package me.jojigarcia.model;
+
+/**
+ * Created by joaquinjimenezgarcia on 4/5/17.
+ */
+public class Avion {
+    private String codVuelo;
+    private String aerolinea;
+    private int velocidad;
+    private date fechaHora;
+    private double distancia;
+}
