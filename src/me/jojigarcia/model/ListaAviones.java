8030c719cdff6eb2446b61524978b15368c46ac1
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-11 16:01:49 +0200

    - Elimina aviones que ya han llegado a pista (distancia = 0.0)

--- src/me/jojigarcia/model/ListaAviones.java
+++ src/me/jojigarcia/model/ListaAviones.java
@@ -2,6 +2,7 @@ package me.jojigarcia.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 /**
  * Created by joaquinjimenezgarcia on 4/5/17.
@@ -22,8 +23,23 @@ public class ListaAviones {
     public void mostrarAviones(){
         Collections.sort(aviones, Avion.comparadorPorDistancia);
 
-        for (Avion avion: aviones){
+        Iterator<Avion> itAvion = aviones.iterator();
+        while (itAvion.hasNext()){
+           Avion avion = itAvion.next();
+
+
+        //for (Avion avion: aviones){
             System.out.println(avion);
+
+            /* Si necesito recorrer una colección y eliminar elementos de la misma
+               no me sirve for each. (No puede hacer esas dos cosas a la vez).
+            if (avion.getDistancia() == 0.0){
+                aviones.remove(avion);
+            }*/
+
+            if (avion.getDistancia() == 0.0){
+                itAvion.remove();
+            }
         }
     }
 
